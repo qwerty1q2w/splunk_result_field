be50aacc479686c0acdab31243ac8fe4ff0379e1
Pushkarev Dmitrii <63014898+qwerty1q2w@users.noreply.github.com>
2024-09-15 21:02:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,13 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # Получаем строку с полями из формы
     input_string = request.form.get('fields')
-    
-    # Разделяем строку по запятым и убираем лишние пробелы
     fields = [field.strip() for field in input_string.split(',')]
     
-    # Формируем шаблон с подстановкой переменных
     result = []
     for field in fields:
-        # Для каждого поля добавляем строку с форматированием
         result.append(f"{field} - $result.{field}$")
     
-    # Объединяем строки в результат, разделяя их переносами строки
     formatted_result = "\n".join(result)
     
     return f"<pre>{formatted_result}</pre>"
